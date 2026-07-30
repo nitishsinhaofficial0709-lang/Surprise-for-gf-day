c68adfa9e584b8f58c02461c2d5381b0d2e1832e
nitishsinhaofficial0709-lang <nitishsinhaofficial0709@gmail.com>
2026-07-31 02:33:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,26 @@
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-    font-family:Arial, Helvetica, sans-serif;
-}
+function openGift() {
+    document.querySelector(".container").innerHTML = `
+        <h1>❤️ Dear Ankita ❤️</h1>
 
-body{
-    background:linear-gradient(to bottom,#020111,#191970,#000);
-    height:100vh;
-    overflow:hidden;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    color:white;
-}
+        <p style="font-size:22px;max-width:700px;margin:auto;line-height:1.8;">
+            From the day you came into my life,
+            every moment became more beautiful.
+            Every smile of yours is my favorite memory,
+            and every heartbeat reminds me how lucky I am.
 
-.container{
-    text-align:center;
-    z-index:10;
-    padding:20px;
-}
+            Thank you for being my happiness,
+            my peace,
+            my best friend,
+            and the love of my life.
 
-h1{
-    font-size:42px;
-    color:#ff6ec7;
-    text-shadow:0 0 20px #ff6ec7;
-    margin:20px 0;
-}
+            Happy Girlfriend's Day ❤️
+        </p>
 
-h3{
-    color:#ffd700;
-    margin-bottom:15px;
-}
+        <br><br>
 
-p{
-    font-size:20px;
-    margin-bottom:30px;
-    color:#eeeeee;
-}
-
-button{
-    padding:15px 35px;
-    border:none;
-    border-radius:40px;
-    background:#ff4d88;
-    color:white;
-    font-size:18px;
-    cursor:pointer;
-    transition:0.4s;
-    box-shadow:0 0 20px #ff4d88;
-}
-
-button:hover{
-    transform:scale(1.08);
-    box-shadow:0 0 35px #ff4d88;
-}
-
-#stars{
-    position:fixed;
-    width:100%;
-    height:100%;
-    background-image:
-    radial-gradient(white 1px, transparent 1px),
-    radial-gradient(white 1px, transparent 1px);
-    background-size:80px 80px;
-    background-position:0 0,40px 40px;
-    animation:twinkle 6s linear infinite;
-}
-
-@keyframes twinkle{
-    0%{opacity:.7;}
-    50%{opacity:1;}
-    100%{opacity:.7;}
+        <h2 style="color:#FFD700;">
+            Forever Yours,<br>
+            Nitish ❤️
+        </h2>
+    `;
 }
